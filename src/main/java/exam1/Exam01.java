92843ec2b16f4c9f95b935b88158bc3119162230
chatupon khooha <khochatupon@gmail.com>
2017-07-21 15:24:27 +0700

    Complete test case 2 of exam

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -1,18 +1,24 @@
 package exam1;
 
 public class Exam01 {
+    Boolean playerAWin = false;
+    Boolean playerBWin = false;
 
     public Exam01(String firstPerson, String secondPerson) {
     }
 
 
     public void playerAWin() {
+        playerAWin = true;
     }
 
     public void playerBWin() {
     }
 
     public String getScore() {
+
+        if (playerAWin)
+            return "Fifteen-Love";
         return "Love-All";
     }
 
