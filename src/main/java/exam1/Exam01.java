0d4036d7cb72041405291d3046a7ed00575dbc07
chatupon khooha <khochatupon@gmail.com>
2017-07-21 16:04:18 +0700

    Complete test case to 24 of exam

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -18,18 +18,43 @@ public class Exam01 {
 
     public String getScore() {
 
-        if (playerAWin==1)
+        if (playerAWin==1 && playerBWin==0){
             return "Fifteen-Love";
-        else if (playerAWin==2)
+        }
+        else if (playerAWin==1 && playerBWin==1)
+            return "Fifteen-All";
+        else if (playerAWin==1 && playerBWin==3)
+            return "Fifteen-Forty";
+        else if (playerAWin==2 && playerBWin==0)
             return "Thirty-Love";
-        else if (playerAWin==3)
+        else if (playerAWin==2 && playerBWin==1)
+            return "Thirty-Fifteen";
+        else if (playerAWin==2 && playerBWin==2)
+            return "Thirty-All";
+        else if (playerAWin==2 && playerBWin==3)
+            return "Thirty-Forty";
+        else if (playerAWin==3 && playerBWin==0)
             return "Forty-Love";
-        else if (playerAWin==4)
+        else if (playerAWin==3 && playerBWin==1)
+            return "Forty-Fifteen";
+        else if (playerAWin==3 && playerBWin==2)
+            return "Forty-Thirty";
+        else if (playerAWin==3 && playerBWin==3)
+            return "Deuce";
+        else if (playerAWin==4 && playerBWin ==3)
+            return "Advantage Player A";
+        else if (playerAWin==4) {
+            if (playerBWin==4)
+                return "Deuce";
             return "Win for Player A";
+        }
         else if (playerBWin==1)
             return "Love-Fifteen";
-        else if (playerBWin==2)
+        else if (playerBWin==2) {
+            if (playerAWin == 1)
+                return "Fifteen-Thirty";
             return "Love-Thirty";
+        }
         else if (playerBWin==3)
             return "Love-Forty";
         else if (playerBWin==4)
